7ac2ac9be2567f1875ce1085afc66080bc2dd70f
Ramakrishna Gajula <Ramakrishna@DESKTOP-J2CG2DM.kuikdelivery.com>
2021-04-14 14:55:31 +0530

    HelloWorld

--- java-examples/src/main/java/HelloWorld.java
+++ java-examples/src/main/java/HelloWorld.java
@@ -1,5 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        SpringApplication.run(HelloWorld.class,args);
+    }
+    @GetMapping("/hello")
+    public String sayhello(@RequestParam(value ="myName", defaultValue ="world")String name){
+         return  String.format("Hello %s!", name);
     }
 }
